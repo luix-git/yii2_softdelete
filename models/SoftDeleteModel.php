248f93e79ad043a3698358139f1b5bd471f0352e
Andrey Vlasov <vlasov.andrey@ucann.ru>
2019-02-27 14:50:14 +0300

    create abstract class SoftDeleteModel

---
+++ models/SoftDeleteModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models;
+
+use \yii\db\ActiveRecord;
+
+/**
+ * Class SoftDeleteModel
+ * @package app\models
+ */
+abstract class SoftDeleteModel extends ActiveRecord
+{
+    /**
+     * @return bool
+     */
+    public function isDeleted(): bool
+    {
+        return isset($this->deleted_at);
+    }
+
+    protected function deleteInternal()
+    {
+        if (!$this->beforeDelete()) {
+            return false;
+        }
+
+        if ($this->isDeleted()) {
+            //Здесь спорный момент: считать ли результат попытки удаления "удалённой" записи успешной.
+            return false;
+        }
+
+        $this->deleted_at = strftime('%Y-%m-%d %T');
+        $result = $this->save();
+
+        $this->setOldAttributes(null);
+        $this->afterDelete();
+
+        return $result;
+    }
+
+    /**
+     * @return bool
+     */
+    public function restore(): bool
+    {
+        if ($this->isDeleted()) {
+            $this->deleted_at = null;
+            $this->save();
+            return true;
+        }
+
+        return false;
+    }
+}
